9709d9fc98f6f9a3e38facfb88dcc0677fbb789d
Jaizon Carlos <jaizoncarlos@protonmail.com>
2022-10-20 19:07:13 -0300

    changed title to "Radio2.0"

--- src/components/Body.svelte
+++ src/components/Body.svelte
@@ -1,27 +1,26 @@
 <script>
-
-    import {radios} from '../stores/RadioStore'
-    import { Card, Player} from './pieces'
-
+    import { radios } from '../stores/RadioStore'
+    import { Card, Player } from './pieces'
 </script>
 
-<main class='w-full flex pb-10 pt-16 sm:pt-10 pr-2 justify-center overflow-hidden h-[100vh]'>
+<main
+    class="w-full flex pb-10 pt-16 sm:pt-10 pr-2 justify-center overflow-hidden h-[100vh]"
+>
     <Player />
     <div class="scroll w-[100vw] overflow-y-scroll overflow-x-hidden pr-3 pl-7">
-        <div class="container grid grid-cols-body gap-3.5 max-w-6xl mx-auto my-6">
+        <div
+            class="container grid grid-cols-body gap-3.5 max-w-6xl mx-auto my-6"
+        >
             {#if $radios}
-            {#each $radios as radio (radio.id)}
-                <Card
-                    {radio}
-                />
-            {/each}
-        {/if}
+                {#each $radios as radio (radio.id)}
+                    <Card {radio} />
+                {/each}
+            {/if}
         </div>
     </div>
 </main>
 
 <style>
-
     /* scroll bar */
     /* width */
     ::-webkit-scrollbar {
@@ -36,8 +35,9 @@
     /* Handle */
     ::-webkit-scrollbar-thumb {
         background: transparent;
-        box-shadow: inset 1px 1px 3px rgb(136, 164, 255), inset -1px -1px 3px rgb(255, 152, 250), 1px 1px 2px rgb(204, 216, 255), -1px -1px 2px rgb(255, 187, 195);
+        box-shadow: inset 1px 1px 3px rgb(136, 164, 255),
+            inset -1px -1px 3px rgb(255, 152, 250),
+            1px 1px 2px rgb(204, 216, 255), -1px -1px 2px rgb(255, 187, 195);
         border-radius: 10px;
     }
-
 </style>
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,24 +1,41 @@
 <script>
-    import {playing, station} from '../stores/RadioStore'
-    import {player, playPause} from './pieces/Player.svelte'
+    import { playing, station } from '../stores/RadioStore'
+    import { player, playPause } from './pieces/Player.svelte'
 
     const handleVolume = (e) => {
         player.volume = e.target.value
     }
-
 </script>
 
-
-<footer class="w-full flex fixed bottom-0 z-1000 bg-white px-5 justify-center overflow-hidden">
-    <div class="w-full max-w-6xl h-full py-2 flex justify-between align-middle ">
-        <div class="flex justify-middle text-center mx-auto w-full h-full align-middle max-w-max">
-            <button disabled={$station.src == ''} class="text-xl mr-5 p-0" on:click={()=> playPause($playing, playing)}>
-                <i class={`text-2xl fa ${!$playing ? 'fa-play' : 'fa-pause'}`}></i>
+<footer
+    class="w-full flex fixed bottom-0 z-1000 bg-white px-5 justify-center overflow-hidden"
+>
+    <div
+        class="w-full max-w-6xl h-full py-2 flex justify-between align-middle "
+    >
+        <div
+            class="flex justify-middle text-center mx-auto w-full h-full align-middle max-w-max"
+        >
+            <button
+                disabled={$station.src == ''}
+                class="text-xl mr-5 p-0"
+                on:click={() => playPause($playing, playing)}
+            >
+                <i
+                    class={`text-2xl fa ${!$playing ? 'fa-play' : 'fa-pause'}`}
+                />
             </button>
             {#if player}
-                    <input class="w-20" min="0" max="1" step="0.1" value={player.volume} type="range" on:change={(e)=> handleVolume(e)}/>
+                <input
+                    class="w-20"
+                    min="0"
+                    max="1"
+                    step="0.1"
+                    value={player.volume}
+                    type="range"
+                    on:change={(e) => handleVolume(e)}
+                />
             {/if}
-            
         </div>
         <div class="w-full h-full text-right my-auto text-sm">
             {#if $station.radio}
